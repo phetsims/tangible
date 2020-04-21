761b8e30b5530e6d995d145c2fb1915882b94eeb
zepumph <michael.kauzmann@colorado.edu>
2020-04-20 17:13:14 -0800

    MarkerInput to run on step, and only initialized once. https://github.com/phetsims/ratio-and-proportion/issues/21

--- js/MarkerInput.js
+++ js/MarkerInput.js
@@ -2,6 +2,12 @@
 
 /**
  * This file is to prototype mechamarkers as an input controller to a phetsim, see https://github.com/phetsims/a11y-research/issues/153
+ *
+ * It specifies the Mechamarkers global as a member to reduce global name space access.
+ *
+ * For usage, most of the time this should be subtyped and then stepped with logic in step that monitors the state of
+ * the marker control via the Mechamarkers global.
+ *
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
@@ -9,34 +15,33 @@
 import tangible from './tangible.js';
 import timer from '../../axon/js/timer.js';
 
+// This flag keeps Mechamarkers from being implemented more than once.
+let mechamarkersInitialized = false;
+
 class MarkerInput {
 
   constructor() {
 
     // @protected (read-only) {Mechamarkers} - instead of using the global, this allows to keep encapsulation better.
     this.Mechamarkers = window.Mechamarkers;
-  }
 
-  /**
-   * Must be called to begin Mechamarkers input. This function wires up to listen to when Mechamarkers updates
-   * @public
-   *
-   * @param {function(Mechamarkers)} updateFunction
-   */
-  beginInput( updateFunction ) {
+    // the Mechamarkers library only needs to be initialized once. If another instance has already done this, then don't
+    // do it again.
+    if ( !mechamarkersInitialized ) {
 
-    timer.addListener( () => {
+      timer.addListener( () => {
 
-      // Mechamarkers stuff
-      this.Mechamarkers.update( Date.now() );
+        // Mechamarkers stuff
+        this.Mechamarkers.update( Date.now() );
+      } );
 
-      updateFunction( this.Mechamarkers );
-    } );
+      const canvas = document.createElement( 'canvas' );
+      const ctx = canvas.getContext( '2d' );
+      document.body.appendChild( canvas );
+      this.Mechamarkers.init( canvas, ctx );
 
-    const canvas = document.createElement( 'canvas' );
-    const ctx = canvas.getContext( '2d' );
-    document.body.appendChild( canvas );
-    this.Mechamarkers.init( canvas, ctx );
+      mechamarkersInitialized = true;
+    }
   }
 }
 
