4ca50078659b6a49035ae2c9dc205742b5eecbdf
phet-dev <phetadmin@gmail.com>
2023-01-03 04:55:59 -0700

    update copyright dates from daily grunt work

--- js/mediaPipe/MediaPipe.ts
+++ js/mediaPipe/MediaPipe.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * Adds the boilerplate needed for MediaPipe "hands" implementation to run in PhET Sims. See https://github.com/phetsims/ratio-and-proportion/issues/431
