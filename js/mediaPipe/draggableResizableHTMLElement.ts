be069afcb68ce1717f7f5bbf06213f25d0b14a59
phet-dev <phetadmin@gmail.com>
2024-03-23 03:29:04 -0600

    update copyright dates from daily grunt work

--- js/mediaPipe/draggableResizableHTMLElement.ts
+++ js/mediaPipe/draggableResizableHTMLElement.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Wire up a draggable/resizable div for the canvas element to live in. Returns that container
